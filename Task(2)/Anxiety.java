c8ef7f8bb89800b9c24bb68ffb65a91a4d4f225c
YasmeenMedhat <36551202+YasmeenMedhat@users.noreply.github.com>
2021-09-06 17:22:39 +0200

    Create Anxiety.java

---
+++ Task(2)/Anxiety.java
@@ -0,0 +1,30 @@
+//This theme is to define your weak points, learn how to control them, and hve more empathy towards people surrounding you
+package anxietyawareness;
+
+
+public class Anxiety {
+private int typeNum;
+private String definition;
+private String symptoms;
+private String treatment;
+private String advice;
+
+public String definition()
+{
+return definition;
+}
+
+public String displaySymptoms()
+{
+return symptoms;
+}
+public String reflectByAdvice()
+{
+  return advice;
+}
+public String bookAnAppointment(String Doctor, int time, String options)
+{
+//offer different online platforms
+return options;
+}
+}
